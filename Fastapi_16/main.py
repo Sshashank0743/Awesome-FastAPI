6733930edf5abe8403bb1edd6ca0dd2017d28cce
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2024-07-10 16:35:09 +0530

    update

--- Fastapi_16/main.py
+++ Fastapi_16/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import Optional
 
 app = FastAPI()
@@ -8,6 +8,19 @@ def hello():
     return {"hello":"world"}
 
 
-@app.get("/fruit")
-def Fruits(fruit_name: Optional[str] = None):
+@app.get("/fruit/")
+def Fruits(fruit_name: Optional[str] = Query(None, max_length=5)):
     return {"This is query parameters additional option":fruit_name}
+
+
+@app.get("/fruit1/")
+def Fruits(fruit_name: str = Query(..., min_length=2, max_length=5)):
+    return {"This is query parameters additional option":fruit_name}
+
+
+@app.get("/fruit2/")
+def Fruits(fruit_name: Optional[str] = Query("orange", min_length=2, max_length=5)):
+    a = 2
+    if fruit_name:
+        print("Optional parameters are available")
+    return {"This is query parameters additional option":a}
